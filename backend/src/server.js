02c684f31132dadeb4f304ca07b3ea9b6f3f50b0
Dhruvil Jethva <jethvadhruvil28@gmail.com>
2025-12-22 22:27:38 +0530

    cors update

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,7 +10,14 @@ dotenv.config();
 const app = express();
 
 // middleware
-app.use(cors());
+//app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use("/auth",router)
 app.use("/notes",Noteroute)
