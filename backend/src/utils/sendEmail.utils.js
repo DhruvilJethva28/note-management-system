339fd1fd2ec8623e34f8cde4ca0724732ca33b7e
Dhruvil Jethva <jethvadhruvil28@gmail.com>
2026-01-02 21:28:48 +0530

    replace node mailer to resend

--- backend/src/utils/sendEmail.utils.js
+++ backend/src/utils/sendEmail.utils.js
@@ -1,19 +1,38 @@
-import nodemailer from "nodemailer"
+import { Resend } from "resend";
 
-const sendEmail=async({to,subject,html})=>{
-    const transporter=nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-            user: process.env.EMAIL_USER, // your email
-            pass: process.env.EMAIL_PASS, // app password
-        }
-    })
-    await transporter.sendMail({
-         from: `"Notes App" <${process.env.EMAIL_USER}>`,
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+const sendEmail = async (to, subject, text) => {
+  await resend.emails.send({
+    from: "onboarding@resend.dev", // works without domain
     to,
     subject,
-    html,
-    })
-}
+    text,
+  });
+};
 
-export default sendEmail
+export default sendEmail;
+
+
+
+
+// import nodemailer from "nodemailer"
+
+// const sendEmail=async({to,subject,html})=>{
+//     const transporter=nodemailer.createTransport({
+//         service:"gmail",
+//         auth:{
+//             user: process.env.EMAIL_USER, // your email
+//             pass: process.env.EMAIL_PASS, // app password
+//         }
+//     })
+//     await transporter.sendMail({
+//          from: `"Notes App" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject,
+//     html,
+//     })
+// }
+
+// export default sendEmail
+
